dc2cc95bc7e8e0d6bec733900758c414d0ad8a19
Felipe  Vieira <cunhafelipe098@gmail.com>
2020-06-04 05:32:50 -0300

    feat: list of cars with pagination

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,17 @@
 import { Router } from 'express'
 import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
+import CarController from './app/controllers/CarController'
 import authMiddleware from './app/middlewares/auth'
 
 const routes = new Router()
 
 routes.post('/users', UserController.store)
 routes.post('/sessions', SessionController.store)
+routes.get('/cars', CarController.index)
 
 routes.use(authMiddleware)
 
 routes.put('/users', UserController.update)
-
-
 
 export default routes
