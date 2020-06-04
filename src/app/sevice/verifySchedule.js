3e545fab39ab9be9e807045886cf92a27ee8e0b1
Felipe  Vieira <cunhafelipe098@gmail.com>
2020-06-04 09:30:27 -0300

    refactor: remove console.log

--- src/app/sevice/verifySchedule.js
+++ src/app/sevice/verifySchedule.js
@@ -11,25 +11,18 @@ export default async (WithdrawalDate, DeliveryDate, CarId) => {
   if(Object.values(schedules).length === 0) {
     return true
   }
-  console.log(schedules);
   schedules.map((schedule) => {
     
     if (isBefore(WithdrawalDate, schedule.withdrawalDate) & isBefore(DeliveryDate, schedule.withdrawalDate) & create == true){
       console.log("passou1");
       return create = true
     }
-
-    console.log(isBefore(schedule.deliveryDate, WithdrawalDate))
-    console.log(isBefore(schedule.deliveryDate, DeliveryDate))
-    console.log(create == true)
     if (isBefore(schedule.deliveryDate, WithdrawalDate) & isBefore(schedule.deliveryDate, DeliveryDate) & create == true){
       console.log("passou2");
       return create = true
     }
-    console.log("passou3");
     create = false
   })
-  console.log("passou4");
   return create
   
 }
